31508fc071eb0d4686b471ee9c61c069ac95ae4a
creativebull <creativebull0103@gmail.com>
2022-10-08 10:25:26 +0900

    convertTemperature

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -7,10 +7,26 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 
 export class TemperatureConverter implements OnInit {
+  celsius: number;
+  fahrenheit: number;
 
   ngOnInit() {
+    this.celsius = 0;
+    this.fahrenheit = 0;
     // C = (F − 32) × 5/9
     // F = C*9/5 + 32
+  }
+
+  converTemperature(e, type) {
+    const temperature = e.target.value;
+    if(type === 'cel') {
+      this.celsius = temperature;
+      this.fahrenheit = (temperature * 9) /5 +32;
+    }
+    else {
+      this.fahrenheit = temperature;
+      this.celsius = (temperature-32) * 9 /5;
+    }
   }
 
 }
